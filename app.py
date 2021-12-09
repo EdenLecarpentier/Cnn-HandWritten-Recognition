f06693ac740b8fd71f0ff2d6d2a0fbb8ad9be7d0
Thefubuking <eden.lecarpentier.simplon@gmail.com>
2021-12-09 16:41:19 +0100

    commit

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
 import streamlit as st
 from numpy.core.fromnumeric import size
-import streamlit as st
 import pandas as pd
 import numpy as np
-
-
-import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-import wget
 import cv2
-
-
 from PIL import Image
-
-
-
 import os
 from tensorflow.keras.models import load_model
-import matplotlib as plt
+
 
 
 
